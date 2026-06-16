3e147b19658ba511649a2593b84ef273258e9c16
Aadhithiya J <aadhithiyajayaraman@gmail.com>
2026-06-16 21:55:48 +0530

    Time: 1 ms (71%), Space: 12.4 MB (86.1%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,15 +1,27 @@
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: float
-        """
-        sn = sorted(nums1 + nums2)
-        n = len(sn)
-        mid = n // 2
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
 
-        if n%2:
-            return sn[mid]
-        else :
-            return (sn[mid-1]+sn[mid])/2.0
+        m, n = len(nums1), len(nums2)
+        l, r = 0, m
+
+        while l <= r:
+            i = (l + r) // 2
+            j = (m + n + 1) // 2 - i
+
+            a1 = float('-inf') if i == 0 else nums1[i - 1]
+            a2 = float('inf') if i == m else nums1[i]
+
+            b1 = float('-inf') if j == 0 else nums2[j - 1]
+            b2 = float('inf') if j == n else nums2[j]
+
+            if a1 <= b2 and b1 <= a2:
+                if (m + n) % 2:
+                    return float(max(a1, b1))
+                return (max(a1, b1) + min(a2, b2)) / 2.0
+
+            elif a1 > b2:
+                r = i - 1
+            else:
+                l = i + 1
