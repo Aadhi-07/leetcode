0436ebf0322fb8202f9f3fa859582e896be375ad
Aadhithiya J <aadhithiyajayaraman@gmail.com>
2026-06-18 15:05:44 +0530

    Time: 1 ms (70.33%), Space: 12.4 MB (85.56%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def permute(self, nums):
+        if len(nums) == 0:
+            return [[]]
+
+        param = self.permute(nums[1:])
+        res = []
+
+        for p in param:
+            for i in range(len(p) + 1):
+                p_copy = p[:]
+                p_copy.insert(i, nums[0])
+                res.append(p_copy)
+
+        return res
