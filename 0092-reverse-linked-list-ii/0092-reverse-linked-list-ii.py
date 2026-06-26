3869e026986d892309b021902884f114712f2d00
Aadhithiya J <aadhithiyajayaraman@gmail.com>
2026-06-26 14:48:57 +0530

    Time: 0 ms (100%), Space: 12.6 MB (78.39%) - LeetHub

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def reverseBetween(self, head, left, right):
+        if not head or left == right:
+            return head
+
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+
+        for _ in range(left - 1):
+            prev = prev.next
+
+        cur = prev.next
+
+        for _ in range(right - left):
+            nxt = cur.next
+            cur.next = nxt.next
+            nxt.next = prev.next
+            prev.next = nxt
+
+        return dummy.next
