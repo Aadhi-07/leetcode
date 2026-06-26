5d29bbfb9f60cbe014b9473a28c8232fc7da73ad
Aadhithiya J <aadhithiyajayaraman@gmail.com>
2026-06-26 18:03:03 +0530

    Time: 0 ms (100%), Space: 12.3 MB (54.67%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        if not digits:
+            return []
+
+        mp = {
+            "2": "abc", "3": "def",
+            "4": "ghi", "5": "jkl",
+            "6": "mno", "7": "pqrs",
+            "8": "tuv", "9": "wxyz"
+        }
+
+        res = []
+
+        def bt(i, path):
+            if i == len(digits):
+                res.append(path)
+                return
+
+            for c in mp[digits[i]]:
+                bt(i + 1, path + c)
+
+        bt(0, "")
+        return res
